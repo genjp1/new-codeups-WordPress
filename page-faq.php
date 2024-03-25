43702baee61129d315dc24f9cae41743ba1f260f
go_ohtawa <go.ohtawa@gmail.com>
2024-03-25 16:15:31 +0900

    WordPress化：price & faqページ作成（カスタムフィールド残し）

--- page-faq.php
+++ page-faq.php
@@ -1,5 +1,81 @@
 <?php get_header(); ?>
 
-page-faq.php
+<!-- sub-mv -->
+<section class="sub-mv">
+    <div class="sub-mv__inner">
+      <div class="sub-mv__title-wrap">
+        <h2 class="sub-mv__main-title">FAQ</h2>
+      </div>
+      <div class="sub-mv__img">
+        <picture>
+          <source media="(min-width: 768px)" srcset="<?php echo get_template_directory_uri() ?>/dist/assets/images/common/mv-faq.jpg" class="sub-mv__img-pc">
+          <img src="<?php echo get_template_directory_uri() ?>/dist/assets/images/common/mv-faq-sp.jpg" class="sub-m__img-sp" alt="5人のダイバーが水面に浮かんでいる様子">
+        </picture>
+      </div>
+    </div>
+  </section>
+
+  <!-- パンくず あとでプラグイン化する。-->
+  <div class="breadcrumb">
+    <div class="inner">
+      <div><a href="index.html">TOP</a> > よくある質問</div>
+    </div>
+  </div>
+
+  <!-- page-faq -->
+  <div class="page-faq top-page-faq">
+    <div class="page-faq__inner inner">
+
+      <div class="faq">
+        <div class="faq__inner">
+          <ul class="faq__list faq-list">
+            <li class="faq-list__item">
+              <p class="faq-list__item-question js-faq-question">ここに質問が入ります。</p>
+              <p class="faq-list__item-answer">
+                ここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入ります
+              </p>
+            </li>
+            <li class="faq-list__item">
+              <p class="faq-list__item-question js-faq-question">ここに質問が入ります。</p>
+              <p class="faq-list__item-answer">
+                ここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入ります
+              </p>
+            </li>
+            <li class="faq-list__item">
+              <p class="faq-list__item-question js-faq-question">ここに質問が入ります。</p>
+              <p class="faq-list__item-answer">
+                ここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入ります
+              </p>
+            </li>
+            <li class="faq-list__item">
+              <p class="faq-list__item-question js-faq-question">ここに質問が入ります。</p>
+              <p class="faq-list__item-answer">
+                ここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入ります
+              </p>
+            </li>
+            <li class="faq-list__item">
+              <p class="faq-list__item-question js-faq-question">ここに質問が入ります。</p>
+              <p class="faq-list__item-answer">
+                ここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入ります
+              </p>
+            </li>
+            <li class="faq-list__item">
+              <p class="faq-list__item-question js-faq-question">ここに質問が入ります。</p>
+              <p class="faq-list__item-answer">
+                ここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入ります
+              </p>
+            </li>
+            <li class="faq-list__item">
+              <p class="faq-list__item-question js-faq-question">ここに質問が入ります。</p>
+              <p class="faq-list__item-answer">
+                ここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入りますここに質問の答えが入ります
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+  </div>
 
 <?php get_footer(); ?>
