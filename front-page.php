57eb472c83b0b84909d37d7f89f3deea508ab203
go_ohtawa <go.ohtawa@gmail.com>
2024-04-04 04:42:18 +0900

    WordPress化：TOPページ、セクション動的化完了

--- front-page.php
+++ front-page.php
@@ -325,6 +325,17 @@ $contact = esc_url( home_url( '/contact/' ) );
     </div>
   </section>
 
+  <!-- サブループ対象のセクション上部（セクションとセクションの間）に配置する -->
+<?php
+  $args = array( 
+    //カスタム投稿のスラッグ名を記述
+    'post_type' => 'voice',
+    //表示する記事の件数を指定
+    'posts_per_page' => 2,
+  );
+  $the_query = new WP_Query($args); if($the_query->have_posts()):
+?>
+
   <!-- voice -->
   <section id="voice" class="voice top-voice">
     <div class="voice__inner inner">
@@ -333,45 +344,52 @@ $contact = esc_url( home_url( '/contact/' ) );
         <h2 class="title__sub">お客様の声</h2>
       </div>
       <ul class="voice__voice-cards voice-cards">
+
+      <!-- ループ処理開始の場所に持っていく -->
+      <?php while ($the_query->have_posts()): $the_query->the_post(); ?>
+
         <li class="voice-cards__card voice-card">
           <div class="voice-card__content">
             <div class="voice-card__info">
               <div class="voice-card__meta">
-                <p class="voice-card__age">20代(女性)</p>
-                <p class="voice-card__tag">ライセンス講習</p>
+
+                <!-- ACF条件分岐 -->
+                <?php if (get_field('age')) : ?>
+                  <p class="voice-card__age"><?php the_field('age'); ?></p>
+                <?php endif; ?>
+
+                <!-- タクソノミー取得 -->
+                <?php $terms = get_the_terms( get_the_ID(), 'voice_category' ); ?>
+                <p class="voice-card__tag">
+                <?php if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
+                $term = array_shift( $terms ); // 配列から最初の項目を取り出す
+                echo $term->name;}?>
+                </p>
+
               </div>
-              <h3 class="voice-card__title">ここにタイトルが入ります。ここにタイトル</h3>
+              <h3 class="voice-card__title"><?php the_title();?></h3>
               <hr class="voice-card__line">
             </div>
             <div class="voice-card__img colorbox">
-              <img src="<?php echo get_template_directory_uri() ?>/dist/assets/images/common/voice1.jpg" alt="麦わら帽子を被った女性の様子" class="voice-card__img-img">
+                <?php if (has_post_thumbnail()): ?>
+                  <!-- 投稿にアイキャッチ画像が有る場合の処理 -->
+                  <?php the_post_thumbnail(); ?>
+                  <?php else: ?>
+                      <img src="<?php echo get_template_directory_uri() ?>/dist/assets/images/common/noimage.jpg" alt="" class="review-card__img-img">
+                <?php endif; ?>
             </div>
           </div>
-          <p class="voice-card__text">ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>
-            ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>
-            ここにテキストが入ります。ここにテキストが入ります。
+          <p class="voice-card__text"><?php the_content();?>
           </p>
         </li>
-        <li class="voice-cards__card voice-card">
-          <div class="voice-card__content">
-            <div class="voice-card__info">
-              <div class="voice-card__meta">
-                <p class="voice-card__age">20代(男性)</p>
-                <p class="voice-card__tag">ファンダイビング</p>
-              </div>
-              <h3 class="voice-card__title">ここにタイトルが入ります。ここにタイトル</h3>
-              <hr class="voice-card__line">
-            </div>
-            <div class="voice-card__img colorbox">
-              <img src="<?php echo get_template_directory_uri() ?>/dist/assets/images/common/voice2.jpg" alt="男性がグッドサインをしている様子" class="voice-card__img-img">
-            </div>
-          </div>
-          <p class="voice-card__text">ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>
-            ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。<br>
-            ここにテキストが入ります。ここにテキストが入ります。
-          </p>
-        </li>
-      </ul>
+
+        <!-- ループ終了の場所に持っていく -->
+        <?php endwhile; wp_reset_postdata(); ?>
+        </ul>
+        <?php else : ?>
+            <p>記事が投稿されていません</p>
+        <?php endif; ?>
+
       <div class="voice__btn">
         <a href="<?php echo $voice; ?>" class="btn"><span>View more</span></a>
       </div>
